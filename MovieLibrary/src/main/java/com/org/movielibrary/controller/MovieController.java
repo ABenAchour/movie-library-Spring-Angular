5127c2d390184bb9c7992d32eff77ee40cd61de4
ABenAchour <ambenachour@vermeg.com>
2019-03-24 11:30:17 +0100

    add service for search rest

--- MovieLibrary/src/main/java/com/org/movielibrary/controller/MovieController.java
+++ MovieLibrary/src/main/java/com/org/movielibrary/controller/MovieController.java
@@ -10,6 +10,7 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,102 +23,49 @@ import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.org.movielibrary.model.Movie;
+import com.org.movielibrary.service.MovieService;
 
 @RestController
+@RequestMapping("/api")
 public class MovieController {
 	
-	private String fileName="data/movies.json";
+	@Autowired
+    private MovieService movieService;
 	
-	@PostMapping("/api/movie/add")
+	@PostMapping("/movie/add")
     Movie newMovie( @RequestBody Movie movie) throws IOException {
 		
 		
-		Gson gson = new GsonBuilder().create();
-		File resource = new ClassPathResource(
-				this.fileName).getFile();
-		List<Movie> data = gson.fromJson(new FileReader(resource),  List.class);		
-		
-		data.add(movie);
-		//gson.toJson(data, new FileWriter(jsonOutput));
-		FileWriter fw = new FileWriter(resource);
-        
-		gson.toJson(data, fw);
-        fw.close();
-		return movie;
+		return movieService.newMovie(movie);
 		
 	}
 	
-	@PostMapping("/api/movie/search")
+	@PostMapping("/movie/search")
 	List<Movie> searchMovie( @RequestBody Movie movie) throws IOException {		
-		Gson gson = new GsonBuilder().create();
-		File resource = new ClassPathResource(
-				this.fileName).getFile();
-		Movie[] data = gson.fromJson(new FileReader(resource),  Movie[].class);		
-		List<Movie> dataNew = new ArrayList<Movie>();
-		for(Movie m : data)
-		{
-			if(m.equals(movie))
-			{
-				dataNew.add(m);
-			}
-		}
-		return dataNew;
+		
+		return movieService.searchMovie(movie);
 		
 	}
 	
-	@RequestMapping(value = "/api/movie/update/{index}", method = RequestMethod.POST)
+	@RequestMapping(value = "/movie/update/{index}", method = RequestMethod.POST)
     Movie updateMovie(@PathVariable("index")  int index , @RequestBody Movie movie) throws IOException, ParseException {
-		
-		
-		Gson gson = new GsonBuilder().create();
-		File resource = new ClassPathResource(
-				this.fileName).getFile();
-		List<Movie> data = gson.fromJson(new FileReader(resource),  List.class);		
-		
-		data.set(index, movie);
-		FileWriter fw = new FileWriter(resource);
-        
-		gson.toJson(data, fw);
-        fw.close();
-		return movie;
+		return movieService.updateMovie(index, movie);
 		
 	}
 	
-	@RequestMapping(value = "/api/movie/delete/{index}", method = RequestMethod.GET)
-	void deleteMovie(@PathVariable("index") int index ) throws IOException {
-		
-		Gson gson = new GsonBuilder().create();
-		File resource = new ClassPathResource(
-				this.fileName).getFile();
-		List<Movie> data = gson.fromJson(new FileReader(resource),  List.class);		
-		
-		data.remove(index);
-		FileWriter fw = new FileWriter(resource);
-        
-		gson.toJson(data, fw);
-        fw.close();
+	@RequestMapping(value = "/movie/delete/{index}", method = RequestMethod.GET)
+	void deleteMovie(@PathVariable("index") int index ) throws IOException {		
+		movieService.deleteMovie(index);
 	}
 	
-	@RequestMapping(value = "/api/movie/get/{index}", method = RequestMethod.GET)
-	Movie getMovie(@PathVariable("index") int index ) throws IOException {
-		
-		Gson gson = new GsonBuilder().create();
-		File resource = new ClassPathResource(
-				this.fileName).getFile();
-		Movie[] data = gson.fromJson(new FileReader(resource),  Movie[].class);
-		
-		return data[index];
-		
+	@RequestMapping(value = "/movie/get/{index}", method = RequestMethod.GET)
+	Movie getMovie(@PathVariable("index") int index ) throws IOException {		
+		return movieService.getMovie(index);		
 	}
 	
-	@GetMapping("/api/movie/list")
-	List<Movie> all() throws IOException {
-		
-		Gson gson = new Gson();
-		File resource = new ClassPathResource(
-				this.fileName).getFile();
-		List<Movie> data = gson.fromJson(new FileReader(resource),  List.class);
-		return data;
+	@GetMapping("/movie/list")
+	List<Movie> all() throws IOException {		
+		return movieService.getAllMovies();
 	}
 
 }
---
+++ MovieLibrary/src/main/java/com/org/movielibrary/service/MovieService.java
@@ -0,0 +1,112 @@
+package com.org.movielibrary.service;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.org.movielibrary.model.Movie;
+
+@Service
+public class MovieService {
+	
+	private String fileName="data/movies.json";
+	
+	public List<Movie>  getAllMovies() throws IOException {
+		
+		Gson gson = new Gson();
+		File resource = new ClassPathResource(
+				this.fileName).getFile();
+		List<Movie> data = gson.fromJson(new FileReader(resource),  List.class);
+		
+		return data;	
+		
+	}
+	
+	public Movie getMovie(int index ) throws IOException {
+		
+		Gson gson = new GsonBuilder().create();
+		File resource = new ClassPathResource(
+				this.fileName).getFile();
+		Movie[] data = gson.fromJson(new FileReader(resource),  Movie[].class);
+		
+		return data[index];		
+	}
+	
+	public void deleteMovie(int index ) throws IOException {
+		
+		Gson gson = new GsonBuilder().create();
+		File resource = new ClassPathResource(
+				this.fileName).getFile();
+		List<Movie> data = gson.fromJson(new FileReader(resource),  List.class);		
+		
+		data.remove(index);
+		FileWriter fw = new FileWriter(resource);
+        
+		gson.toJson(data, fw);
+        fw.close();
+	}
+	
+	   public Movie updateMovie( int index , Movie movie) throws IOException, ParseException {
+			
+			
+			Gson gson = new GsonBuilder().create();
+			File resource = new ClassPathResource(
+					this.fileName).getFile();
+			List<Movie> data = gson.fromJson(new FileReader(resource),  List.class);		
+			
+			data.set(index, movie);
+			FileWriter fw = new FileWriter(resource);
+	        
+			gson.toJson(data, fw);
+	        fw.close();
+			return movie;
+			
+		}
+	   
+	  public  List<Movie> searchMovie( Movie movie) throws IOException {		
+			Gson gson = new GsonBuilder().create();
+			File resource = new ClassPathResource(
+					this.fileName).getFile();
+			Movie[] data = gson.fromJson(new FileReader(resource),  Movie[].class);		
+			List<Movie> dataNew = new ArrayList<Movie>();
+			for(Movie m : data)
+			{
+				if(m.equals(movie))
+				{
+					dataNew.add(m);
+				}
+			}
+			return dataNew;
+			
+		}
+	  
+	  public Movie newMovie( Movie movie) throws IOException {
+			
+			
+			Gson gson = new GsonBuilder().create();
+			File resource = new ClassPathResource(
+					this.fileName).getFile();
+			List<Movie> data = gson.fromJson(new FileReader(resource),  List.class);		
+			
+			data.add(movie);
+			//gson.toJson(data, new FileWriter(jsonOutput));
+			FileWriter fw = new FileWriter(resource);
+	        
+			gson.toJson(data, fw);
+	        fw.close();
+			return movie;
+			
+		}
+
+}
